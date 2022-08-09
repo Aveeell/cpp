247e877dacf547bcad478403dd7365021dc191a4
Jimmie Errok <jerrok@at-n5.msk.21-school.ru>
2022-08-09 11:17:58 +0300

    fix abstract class in cpp04

--- cpp04/ex02/AAnimal.cpp
+++ cpp04/ex02/AAnimal.cpp
@@ -6,30 +6,11 @@
 /*   By: jerrok <jerrok@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 11:55:15 by jerrok            #+#    #+#             */
-/*   Updated: 2022/08/08 11:55:16 by jerrok           ###   ########.fr       */
+/*   Updated: 2022/08/09 11:05:32 by jerrok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "AAnimal.hpp"
-
-AAnimal::AAnimal()
-{
-	type = "AAnimal";
-	std::cout << "Default constructor AAnimal" << std::endl;
-}
-
-AAnimal::AAnimal(const AAnimal &old)
-{
-	std::cout << "Copy constructor AAnimal" << std::endl;
-	this->type = old.type;
-}
-
-AAnimal& AAnimal::operator= (const AAnimal &old)
-{
-	if (this != &old)
-		this->type = old.type;
-	return *this;
-}
 
 AAnimal::~AAnimal()
 {
--- cpp04/ex02/AAnimal.hpp
+++ cpp04/ex02/AAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: jerrok <jerrok@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 11:55:17 by jerrok            #+#    #+#             */
-/*   Updated: 2022/08/08 11:55:18 by jerrok           ###   ########.fr       */
+/*   Updated: 2022/08/09 11:05:35 by jerrok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,6 @@ class AAnimal
 	protected:
 		std::string type;
 	public:
-		AAnimal();
-		AAnimal(const AAnimal &old);
-		AAnimal &operator= (const AAnimal &old);
 		virtual ~AAnimal();
 		virtual void makeSound() const = 0;
 		virtual std::string getType() const;
