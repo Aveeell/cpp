88df951c3bbf7cc574d3f5b396e22c7de0cdddd0
Jimmie Errok <jerrok@at-m7.msk.21-school.ru>
2022-08-16 14:13:23 +0300

    done ex01

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,34 +6,51 @@
 /*   By: jerrok <jerrok@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 12:33:57 by jerrok            #+#    #+#             */
-/*   Updated: 2022/08/15 13:29:53 by jerrok           ###   ########.fr       */
+/*   Updated: 2022/08/16 14:12:53 by jerrok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-typedef unsigned int uintptr_t;
-
 struct Data
 {
-	int val = 13;
-	std::string str = "kekw";
+	int val;
+	std::string str;
+	char c;
 };
 
 uintptr_t serialize(Data *ptr)
 {
-	uintptr_t addr;
-	
-	return addr;
+	return reinterpret_cast<uintptr_t>(ptr);
 }
 
 Data *deserialize(uintptr_t raw)
 {
-	
+	return reinterpret_cast<Data *>(raw);
 }
 
-int main()
+int	main()
 {
+	Data *test = new Data;
+	test->val = 13;
+	test->str = "kekw";
+	test->c = 'a';
 	
-	return 0;
+	std::cout << "address: " << test << std::endl;
+	std::cout << "val: " << test->val << std::endl;
+	std::cout << "str: " << test->str << std::endl;
+	std::cout << "c: " << test->c << std::endl << std::endl;
+
+	std::cout << "------------------serialization--------------------" << std::endl;
+	uintptr_t buffer = serialize(test);
+	std::cout << test << " -> " << buffer << std::endl;
+	std::cout << "---------------------------------------------------" << std::endl << std::endl;
+
+	std::cout << "-----------------deserialization-------------------" << std::endl;
+	Data *testDeserialize = deserialize(buffer);
+	std::cout << "address: " << testDeserialize << std::endl;
+	std::cout << "val: " << testDeserialize->val << std::endl;
+	std::cout << "str: " << testDeserialize->str << std::endl;
+	std::cout << "c: " << testDeserialize->c << std::endl;
+	delete test;
 }
